40d961bd79b6f3a76cb834fd3cfb9065b085b67c
Orxan Aliyev <orxanaliyev49@mail.ru>
2021-03-02 17:47:59 +0400

    vue products list

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ProductLists :productsPropsName="productList" @delete:product="deleteProduct" />
+  </div>
+</template>
+
+<script>
+import ProductLists from "./components/ProductLists"
+export default {
+  components:{
+    ProductLists
+  },
+  data(){
+    return{
+      productList:[
+        {id:"1",categoryId:"1",productName:"Acer",about:"Acer Notebook",price:"1500$",stoks:"17"},
+        {id:"2",categoryId:"1",productName:"Toshiba",about:"Toshiba Notebook",price:"800$",stoks:"12"},
+        {id:"3",categoryId:"1",productName:"Macbook",about:"Macbook Notebook",price:"2500$",stoks:"4"},
+        {id:"4",categoryId:"2",productName:"Samsung",about:"Samsung Television",price:"1199$",stoks:"5"}
+      ]
+    }
+  },
+  methods:{
+    deleteProduct(product){
+      this.productList=this.productList.filter(
+        productToFilter=>productToFilter.id!==product.id
+      )
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ProductLists.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+       <p v-if="productsPropsName.length==0" class="productEmpty">Product Lists is empty!</p>
+       <table v-else class="table">
+           <thead>
+               <tr>
+                   <th>Id</th>
+                   <th>Product Name</th>
+                   <th>Category</th>
+                   <th>About</th>
+                   <th>Price</th>
+                   <th>Stoks</th>
+                   <th></th>
+               </tr>
+           </thead>
+           <tbody>
+               <tr v-for="product in productsPropsName" :key="product.id">
+                   <td> {{ product.id }} </td>
+                   <td> {{ product.categoryId }} </td>
+                   <td> {{ product.productName }} </td>
+                   <td> {{ product.about }} </td>
+                   <td> {{ product.price }} </td>
+                   <td> {{ product.stoks }} </td>
+                   <td> <button class="btn btn-sm btn-danger" @click="handleDelete(product)">Delete</button> </td>
+               </tr>
+           </tbody>
+       </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["productsPropsName"],
+    methods:{
+        handleDelete(product){
+            this.$emit("delete:product",product)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .productEmpty{
+        text-align: center;
+        margin-top: 30px;
+        background-color: red;
+        font-weight: 700;
+        font-size: 26px;
+    }
+</style>
